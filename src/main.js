259d5e07fef03c8829fbb9041003892c903344d4
Geovanna Otoni <geovannaotoni@gmail.com>
2023-03-13 20:23:14 -0300

    finalizacao

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// https://www.npmjs.com/package/nanoid
+import { nanoid } from 'nanoid'
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
